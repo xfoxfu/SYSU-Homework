270de7c966c2d2dd2d1a822553b103e4076f974c
Yuze Fu <i@xfox.me>
2020-05-05 14:49:05 +0800

    os: bootable os

--- DCS216/boot.asm
+++ DCS216/boot.asm
@@ -1,7 +1,85 @@
-DISP_BASE_X equ 40
-DISP_BASE_Y equ 12
-DISP_MAX_X  equ 80
-DISP_MAX_Y  equ 25
-DISP_BASE   equ 0xB800
+bits 16
+org  0x7c00
 
-%include "./display.inc"
+LOAD_SECTION equ 0x800
+DRIVER_OFFSET equ 0x100
+
+_start:
+	mov ax, cs ; 通过AX中转, 将CS的值传送给DS和ES
+	mov ds, ax
+	mov es, ax
+
+    ; ; Set SS and SP as they may get used by BIOS calls.
+    ; xor ax, ax
+    ; mov ss, ax
+    ; mov sp, 0x0000
+
+
+    ; cli
+
+
+    ; Get input to %al
+    mov ah, 0x00
+    int 0x16
+
+    ; mov cl, al
+    ; sub cl, 'a' - 2
+    ; inc al
+	cmp al ,'a'
+	je pa
+	cmp al ,'b'
+	je pb
+	cmp al ,'c'
+	je pc
+	cmp al ,'d'
+	je pd
+pa:
+	mov cl,02h
+	jmp read
+pb:
+	mov cl,03h
+	jmp read
+pc:
+	mov cl,04h
+	jmp read
+pd:
+	mov cl,05h
+	jmp read	
+
+read:
+    ; DOS 1+ - SELECT DEFAULT DRIVE
+    ; 
+    ; AH = 0Eh
+    ; DL = new default drive (00h = A:, 01h = B:, etc)
+    mov ah, 0Eh
+    int 21h
+    ; DISK - READ SECTOR(S) INTO MEMORY
+    ; 
+    ; AH = 02h
+    ; AL = number of sectors to read (must be nonzero)
+    ; CH = low eight bits of cylinder number
+    ; CL = sector number 1-63 (bits 0-5)
+    ; high two bits of cylinder (bits 6-7, hard disk only)
+    ; DH = head number
+    ; DL = drive number (bit 7 set for hard disk)
+    ; ES:BX -> data buffer
+    ; 
+	mov ax, LOAD_SECTION
+	mov es, ax
+	mov bx, DRIVER_OFFSET
+	mov ax,0201h
+	; mov dx,0080h
+    mov dh, 00h
+    ; DL read by others
+	mov ch,00h
+	
+	int 13h
+    ; mov ax, LOAD_SECTION
+    ; mov es, ax
+	; jmp [es:100h]
+    jmp (LOAD_SECTION << 4 + DRIVER_OFFSET)
+
+    hlt
+
+times 510 - ($-$$) db 0 ; pad remaining 510 bytes with zeroes
+dw 0xaa55 ; magic bootloader magic - marks this 512 byte sector bootable!
