e8f778bc2ea2c9381851d01b6545779a7e55f316
Yuze Fu <i@xfox.me>
2020-09-16 20:26:38 +0800

    ai: lab1: refactor code

--- DCS311/lab1/src/bin/knn_cat.rs
+++ DCS311/lab1/src/bin/knn_cat.rs
@@ -1,89 +1,16 @@
-use std::collections::{BTreeMap, BinaryHeap, HashMap};
-use std::vec::Vec;
+use lab1::{Case, DistanceObject, ExactEmotion as Emotion};
+use std::collections::{BinaryHeap, HashMap};
 
-fn passage_vector<'a>(passage: &[&'a str]) -> BTreeMap<&'a str, usize> {
-    let mut word_count = BTreeMap::new();
-    for word in passage.iter() {
-        if let Some(c) = word_count.get_mut(*word) {
-            *c += 1;
-        } else {
-            word_count.insert(*word, 1);
-        }
-    }
-
-    word_count
-}
-
-fn safe_sub_abs(lhs: usize, rhs: usize) -> usize {
-    if lhs > rhs {
-        lhs - rhs
-    } else {
-        rhs - lhs
-    }
-}
-
-fn passage_distance(distance_p: u32, lhs: &[&str], rhs: &[&str]) -> usize {
-    let lvec = passage_vector(lhs);
-    let rvec = passage_vector(rhs);
-
-    if distance_p > 0 {
-        let mut diff = 0usize;
-
-        for (lw, lc) in lvec.iter() {
-            if rvec.contains_key(lw) {
-                diff += safe_sub_abs(*lc, rvec[lw]).pow(distance_p);
-            } else {
-                diff += lc.pow(distance_p);
-            }
-        }
-        for (rw, rc) in rvec.iter() {
-            if !lvec.contains_key(rw) {
-                diff += rc.pow(distance_p);
-            }
-        }
-
-        diff
-    } else {
-        let mut p = 0;
-        for w in lvec.keys() {
-            if rvec.contains_key(w) {
-                p += lvec[w] * rvec[w];
-            }
-        }
-        let q: usize = lvec.values().map(|v| v * v).sum::<usize>()
-            * rvec.values().map(|v| v * v).sum::<usize>();
-        ((1f64 - p as f64 / (q as f64).sqrt()) * 1_000_000f64) as usize
-    }
-}
-
-fn predict_emotion<'a>(
+fn predict_emotion(
     threshold_k: usize,
     distance_p: u32,
-    train_data: &'a BTreeMap<Vec<&str>, &str>,
-    target_passage: &'a [&str],
-) -> &'a str {
-    let mut k_minimals = BinaryHeap::<(usize, &str)>::new();
-
-    for (compare_passage, compare_emotion) in train_data.iter() {
-        let dist = passage_distance(distance_p, &target_passage, compare_passage);
-
-        if let Some((max_dist, _)) = k_minimals.peek() {
-            if *max_dist <= dist {
-                continue;
-            }
-        }
-
-        k_minimals.push((dist, compare_emotion));
-
-        if k_minimals.len() > threshold_k {
-            k_minimals.pop();
-        }
-
-        assert!(k_minimals.len() <= threshold_k);
-    }
+    train_data: &[Case<Emotion>],
+    target_passage: &Case<Emotion>,
+) -> Emotion {
+    let k_minimals = target_passage.predict_emotion(threshold_k, distance_p, train_data);
 
     let mut assume_emotions = HashMap::new();
-    for (_dist, emotion) in k_minimals.into_iter() {
+    for DistanceObject(_dist, emotion) in k_minimals.into_iter() {
         if assume_emotions.contains_key(emotion) {
             *assume_emotions.get_mut(emotion).unwrap() += 1;
         } else {
@@ -93,54 +20,24 @@ fn predict_emotion<'a>(
 
     let (emotion, _) = assume_emotions.into_iter().max_by_key(|(_, c)| *c).unwrap();
 
-    emotion
+    emotion.clone()
 }
 
 fn compute_with_kp<W: std::io::Write>(
-    train_file: &str,
-    validation_file: &str,
+    train_data: &[Case<Emotion>],
+    validation_data: &[Case<Emotion>],
     test_file: &str,
     output_file: Option<&mut W>,
     threshold_k: usize,
     distance_p: u32,
 ) -> Result<f64, std::io::Error> {
-    let mut train_data = BTreeMap::new();
-
-    for line in train_file.split('\n') {
-        if line == "Words (split by space),label" || line.is_empty() {
-            continue;
-        }
-
-        let passage = line.split(',').next().unwrap();
-        let emotion = line.split(',').nth(1).unwrap();
-
-        let words = passage.split(' ').collect::<Vec<_>>();
-
-        train_data.insert(words, emotion);
-    }
-
-    let validation_total = validation_file
-        .split('\n')
-        .filter(|s| !s.trim().is_empty() && s != &"Words (split by space),label")
-        .count();
+    let validation_total = validation_data.len();
     let mut validation_ok_count = 0usize;
 
-    for line in validation_file.split('\n') {
-        if line == "Words (split by space),label" || line.is_empty() {
-            continue;
-        }
-
-        let target_passage = line
-            .split(',')
-            .next()
-            .unwrap()
-            .split(' ')
-            .collect::<Vec<_>>();
-        let target_emotion = line.split(',').nth(1).unwrap();
-
+    for target_passage in validation_data.iter() {
         let emotion = predict_emotion(threshold_k, distance_p, &train_data, &target_passage);
 
-        if emotion == target_emotion {
+        if emotion == target_passage.emotion {
             validation_ok_count += 1;
         }
     }
@@ -159,14 +56,11 @@ fn compute_with_kp<W: std::io::Write>(
             }
 
             let id = line.split(',').next().unwrap();
-            let target_passage = line
-                .split(',')
-                .nth(1)
-                .unwrap()
-                .split(' ')
-                .collect::<Vec<_>>();
+            let passage = line.split(',').nth(1).unwrap();
 
-            let emotion = predict_emotion(threshold_k, distance_p, &train_data, &target_passage);
+            let case = Case::new(passage);
+
+            let emotion = predict_emotion(threshold_k, distance_p, &train_data, &case);
 
             writeln!(
                 fout,
@@ -197,12 +91,15 @@ fn main() -> Result<(), std::io::Error> {
             .unwrap_or_else(|| "./lab1_data/classification_dataset/test_set.csv".to_string()),
     )?;
 
+    let train_data = Case::<Emotion>::parse_from_file(&train_file);
+    let validation_data = Case::<Emotion>::parse_from_file(&validation_file);
+
     let mut best_ks = BinaryHeap::with_capacity(100);
     for p in 0..5 {
         for k in 1..20 {
             let accuracy = compute_with_kp::<std::fs::File>(
-                &train_file,
-                &validation_file,
+                &train_data,
+                &validation_data,
                 &test_file,
                 None,
                 k,
@@ -216,8 +113,8 @@ fn main() -> Result<(), std::io::Error> {
 
     let mut output = std::fs::File::create("17341039_FUYuze_KNN_classification.csv")?;
     compute_with_kp(
-        &train_file,
-        &validation_file,
+        &train_data,
+        &validation_data,
         &test_file,
         Some(&mut output),
         best_k,
--- DCS311/lab1/src/bin/knn_regres.rs
+++ DCS311/lab1/src/bin/knn_regres.rs
@@ -1,143 +1,7 @@
-use std::collections::{BinaryHeap, HashMap};
+use lab1::ProbEmotion as Emotion;
+use lab1::{Case, DistanceObject};
+use std::collections::BinaryHeap;
 use std::vec::Vec;
-
-#[derive(PartialEq, Clone)]
-struct Emotion {
-    pub anger: f64,
-    pub disgust: f64,
-    pub fear: f64,
-    pub joy: f64,
-    pub sad: f64,
-    pub surprise: f64,
-}
-
-impl Emotion {
-    pub fn new(anger: f64, disgust: f64, fear: f64, joy: f64, sad: f64, surprise: f64) -> Self {
-        Self {
-            anger,
-            disgust,
-            fear,
-            joy,
-            sad,
-            surprise,
-        }
-    }
-
-    pub fn from_vec(v: Vec<f64>) -> Self {
-        Self::new(v[0], v[1], v[2], v[3], v[4], v[5])
-    }
-
-    pub fn into_vec(self) -> Vec<f64> {
-        vec![
-            self.anger,
-            self.disgust,
-            self.fear,
-            self.joy,
-            self.sad,
-            self.surprise,
-        ]
-    }
-}
-
-struct Case<'a> {
-    pub passage: HashMap<&'a str, usize>,
-    pub emotion: Emotion,
-}
-
-impl<'a> Case<'a> {
-    pub fn new(passage: &'a str) -> Self {
-        let mut word_count = HashMap::new();
-        for word in passage.split(' ') {
-            if let Some(c) = word_count.get_mut(word) {
-                *c += 1;
-            } else {
-                word_count.insert(word, 1);
-            }
-        }
-
-        Self {
-            passage: word_count,
-            emotion: Emotion {
-                anger: 0f64,
-                disgust: 0f64,
-                fear: 0f64,
-                joy: 0f64,
-                sad: 0f64,
-                surprise: 0f64,
-            },
-        }
-    }
-
-    pub fn new_with_emotion(
-        passage: &'a str,
-        anger: f64,
-        disgust: f64,
-        fear: f64,
-        joy: f64,
-        sad: f64,
-        surprise: f64,
-    ) -> Self {
-        let mut s = Self::new(passage);
-
-        s.emotion.anger = anger;
-        s.emotion.disgust = disgust;
-        s.emotion.fear = fear;
-        s.emotion.joy = joy;
-        s.emotion.sad = sad;
-        s.emotion.surprise = surprise;
-
-        s
-    }
-
-    pub fn from_line(line: &'a str) -> Self {
-        let mut iter = line.split(',');
-        let words = iter.next().unwrap();
-        let anger = iter.next().and_then(|v| v.parse().ok()).unwrap();
-        let disgust = iter.next().and_then(|v| v.parse().ok()).unwrap();
-        let fear = iter.next().and_then(|v| v.parse().ok()).unwrap();
-        let joy = iter.next().and_then(|v| v.parse().ok()).unwrap();
-        let sad = iter.next().and_then(|v| v.parse().ok()).unwrap();
-        let surprise = iter.next().and_then(|v| v.parse().ok()).unwrap();
-
-        Self::new_with_emotion(words, anger, disgust, fear, joy, sad, surprise)
-    }
-
-    pub fn parse_from_file(file: &'a str) -> Vec<Self> {
-        file.split('\n')
-            .filter(|l| {
-                *l != "Words (split by space),anger,disgust,fear,joy,sad,surprise" && !l.is_empty()
-            })
-            .map(Self::from_line)
-            .collect()
-    }
-
-    pub fn distance(&self, rhs: &Case<'a>, dist_p: u32) -> usize {
-        if dist_p != 0 {
-            let mut diff = 0usize;
-            for w in self.passage.keys().chain(rhs.passage.keys()) {
-                if self.passage.contains_key(w) && rhs.passage.contains_key(w) {
-                    diff +=
-                        (self.passage.get(w).unwrap() - rhs.passage.get(w).unwrap()).pow(dist_p);
-                } else if self.passage.contains_key(w) {
-                    diff += self.passage.get(w).unwrap().pow(dist_p);
-                } else if rhs.passage.contains_key(w) {
-                    diff += rhs.passage.get(w).unwrap().pow(dist_p);
-                }
-            }
-            diff
-        } else {
-            let mut p = 0;
-            for w in self.passage.keys() {
-                if rhs.passage.contains_key(w) {
-                    p += self.passage[w] * rhs.passage[w];
-                }
-            }
-            let q: usize = self.passage.values().map(|v| v * v).sum::<usize>()
-                * rhs.passage.values().map(|v| v * v).sum::<usize>();
-            ((1f64 - p as f64 / (q as f64).sqrt()) * 1_000_000f64) as usize
-        }
-    }
-}
 
 fn correlation_coefficient_single<'a, F>(
     emotions: impl Iterator<Item = (&'a Emotion, &'a Emotion)> + Clone,
@@ -168,46 +32,13 @@ fn correlation_coefficient<'a>(
         / 6f64
 }
 
-#[derive(PartialEq)]
-struct DistanceObject<'a>(usize, &'a Emotion);
-impl<'a> std::cmp::Eq for DistanceObject<'a> {}
-impl<'a> std::cmp::PartialOrd for DistanceObject<'a> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.0.partial_cmp(&other.0)
-    }
-}
-impl<'a> std::cmp::Ord for DistanceObject<'a> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.0.cmp(&other.0)
-    }
-}
-
 fn predict_emotion(
     threshold_k: usize,
     distance_p: u32,
-    train_data: &[Case],
-    target_passage: &Case,
+    train_data: &[Case<Emotion>],
+    target_passage: &Case<Emotion>,
 ) -> Emotion {
-    let mut k_minimals = BinaryHeap::<DistanceObject>::new();
-
-    for case in train_data.iter() {
-        let dist = target_passage.distance(case, distance_p);
-
-        if let Some(DistanceObject(max_dist, _)) = k_minimals.peek() {
-            if *max_dist <= dist {
-                continue;
-            }
-        }
-
-        k_minimals.push(DistanceObject(dist, &case.emotion));
-
-        if k_minimals.len() > threshold_k {
-            k_minimals.pop();
-        }
-
-        assert!(k_minimals.len() <= threshold_k);
-    }
-    // assert!(k_minimals.len() == threshold_k);
+    let k_minimals = target_passage.predict_emotion(threshold_k, distance_p, train_data);
 
     let mut emotion_vec = Vec::with_capacity(6);
     emotion_vec.resize(6, 0f64);
@@ -231,17 +62,13 @@ fn predict_emotion(
 }
 
 fn compute_with_kp<W: std::io::Write>(
-    train_file: &str,
-    validation_file: &str,
+    train_data: &[Case<Emotion>],
+    validation_data: &[Case<Emotion>],
     test_file: &str,
     output_file: Option<&mut W>,
     threshold_k: usize,
     distance_p: u32,
 ) -> Result<f64, std::io::Error> {
-    let train_data = Case::parse_from_file(train_file);
-
-    let validation_data = Case::parse_from_file(validation_file);
-
     let predicted_emotions = validation_data
         .iter()
         .map(|case| predict_emotion(threshold_k, distance_p, &train_data, case))
@@ -255,15 +82,10 @@ fn compute_with_kp<W: std::io::Write>(
 
     println!(
         "P = {}, K = {}, Accuracy = {}",
-        distance_p,
-        threshold_k,
-        // validation_ok_count as f64 / validation_total as f64
-        coefficient
+        distance_p, threshold_k, coefficient
     );
 
     if let Some(fout) = output_file {
-        // TODO:
-
         for line in test_file.split('\n') {
             if line == "textid,Words (split by space),anger,disgust,fear,joy,sad,surprise"
                 || line.is_empty()
@@ -313,12 +135,15 @@ fn main() -> Result<(), std::io::Error> {
             .unwrap_or_else(|| "./lab1_data/regression_dataset/test_set.csv".to_string()),
     )?;
 
+    let train_data = Case::<Emotion>::parse_from_file(&train_file);
+    let validation_data = Case::<Emotion>::parse_from_file(&validation_file);
+
     let mut best_ks = BinaryHeap::with_capacity(100);
     for p in 0..5 {
         for k in 1..20 {
             let accuracy = compute_with_kp::<std::fs::File>(
-                &train_file,
-                &validation_file,
+                &train_data,
+                &validation_data,
                 &test_file,
                 None,
                 k,
@@ -332,8 +157,8 @@ fn main() -> Result<(), std::io::Error> {
 
     let mut output = std::fs::File::create("17341039_FUYuze_KNN_regression.csv")?;
     compute_with_kp(
-        &train_file,
-        &validation_file,
+        &train_data,
+        &validation_data,
         &test_file,
         Some(&mut output),
         best_k,
