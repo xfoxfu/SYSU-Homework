42b671d3455f299802076239b9ef0c3ff747e0e7
Yuze Fu <i@xfox.me>
2020-11-15 09:25:03 +0800

    graphics: asgn5: import code

---
+++ DCS304/asgn5/PathTracer.cpp
@@ -0,0 +1,77 @@
+#include "PathTracer.h"
+
+#include <time.h>
+#include <iostream>
+
+PathTracer::PathTracer()
+	: m_channel(4), m_width(800), m_height(600), m_image(nullptr) {}
+
+PathTracer::~PathTracer()
+{
+	if (m_image != nullptr)
+		m_image;
+	m_image = nullptr;
+}
+
+void PathTracer::initialize(int width, int height)
+{
+	m_width = width;
+	m_height = height;
+	if (m_image != nullptr)
+		delete m_image;
+
+	// allocate pixel buffer, RGBA format.
+	m_image = new unsigned char[width * height * m_channel];
+}
+
+unsigned char * PathTracer::render(double & timeConsuming)
+{
+	if (m_image == nullptr)
+	{
+		std::cout << "Must call initialize() before rendering.\n";
+		return nullptr;
+	}
+
+	// record start time.
+	double startFrame = clock();
+
+	// render the image pixel by pixel.
+	for (int row = m_height - 1; row >= 0; --row)
+	{
+		for (int col = 0; col < m_width; ++col)
+		{
+			// TODO: implement your ray tracing algorithm by yourself.
+			vec3 color;
+			color[0] = static_cast<float>(col) / static_cast<float>(m_width);
+			color[1] = static_cast<float>(row) / static_cast<float>(m_height);
+			color[2] = 0.2;
+			drawPixel(col, row, color);
+		}
+	}
+
+	// record end time.
+	double endFrame = clock();
+
+	// calculate time consuming.
+	timeConsuming = static_cast<double>(endFrame - startFrame) / CLOCKS_PER_SEC;
+
+	return m_image;
+}
+
+void PathTracer::drawPixel(unsigned int x, unsigned int y, const vec3 & color)
+{
+	// Check out 
+	if (x < 0 || x >= m_width || y < 0 || y >= m_height)
+		return;
+	// x is column's index, y is row's index.
+	unsigned int index = (y * m_width + x) * m_channel;
+	// store the pixel.
+	// red component.
+	m_image[index + 0] = static_cast<unsigned char>(255 * color.x());
+	// green component.
+	m_image[index + 1] = static_cast<unsigned char>(255 * color.y());
+	// blue component.
+	m_image[index + 2] = static_cast<unsigned char>(255 * color.z());
+	// alpha component.
+	m_image[index + 3] = static_cast<unsigned char>(255);
+}
