c4dcae6fb13d57dd4aeb3f2fd230acc2bffa3b3e
Yuze Fu <i@xfox.me>
2020-12-13 12:08:38 +0800

    graphics: asgn7: import code

---
+++ DCS304/asgn7/src/rope.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+#include "CGL/vector2D.h"
+
+#include "mass.h"
+#include "rope.h"
+#include "spring.h"
+
+namespace CGL {
+
+    Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+    {
+        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+
+//        Comment-in this part when you implement the constructor
+//        for (auto &i : pinned_nodes) {
+//            masses[i]->pinned = true;
+//        }
+    }
+
+    void Rope::simulateEuler(float delta_t, Vector2D gravity)
+    {
+        for (auto &s : springs)
+        {
+            // TODO (Part 2): Use Hooke's law to calculate the force on a node
+        }
+
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                // TODO (Part 2): Add gravity and global damping, then compute the new velocity and position
+
+
+            }
+
+            // Reset all forces on each mass
+            m->forces = Vector2D(0, 0);
+        }
+    }
+
+    void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+    {
+        for (auto &s : springs)
+        {
+            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            // 在此进行质点的位置调整，维持弹簧的原始长度
+        }
+
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                Vector2D temp_position = m->position;
+                // TODO (Part 3): Set the new position of the rope mass
+                // 在此计算重力影响下，质点的位置变化
+
+            }
+        }
+    }
+}
