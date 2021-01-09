5217e61c3e035e618f777d85582d09ebec1a7803
Zhenggehan <31180310+zeblooder@users.noreply.github.com>
2021-01-09 19:58:32 +0800

    test purchase

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
-#include <iostream>
 #include "mysqlclient.h"
+#include "sell.h"
 #include "stock.h"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
     // 后面直接使用client::update, query方法即可执行增删查改
     std::string host, port, user, password, database;
     if (argc != 6)
-        return 0;
+    {
+        host = "localhost";
+        port = "3306";
+        user = "root";
+        password = "0000";
+        database = "bookstore";
+    }
+    else
+    {
+        host = argv[1], port = argv[2], user = argv[3], password = argv[4], database = argv[5];
+    }
 
-    host = argv[1], port = argv[2], user = argv[3], password = argv[4], database = argv[5];
     MySQLClient client(host.c_str(), atoi(port.c_str()), user.c_str(), password.c_str(), database.c_str());
-
+    for (int i = 1; i < 10; i++)
+    {
+        purchase(client, i, std::string("zhenggehan"), 1);
+    }
     /*
     std::string command = select_command(
         {
